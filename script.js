a7c80a0155d282a1bba8646c468cc5b52c395288
Rabindranath Ferreira <ferreiralox@gmail.com>
2017-12-17 19:17:02 -0300

    for loop structure

--- script.js
+++ script.js
@@ -3,17 +3,6 @@
 // genera un numero entre 1 y 5 generado de manera aleatoria y debe ser rendondiado
 var sign = prompt("what is your astrological sign?" );
 
-switch (sign) {
-    case "tauro":
-        alert("Tauro time");
-        break;
-    case "sagitario":
-        alert("Sagitario time");
-        break;
-    case "escorpion":
-        alert("Escorpion time");
-        break;
-    default:
-        alert("Default result");
-        break;
+for (let index = 0; index < 10; index++) {
+    console.log(index);
 }
