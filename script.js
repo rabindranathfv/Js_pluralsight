b947977e0b46f28a6d52cdebadbc4657d6b71834
Rabindranath Ferreira <ferreiralox@gmail.com>
2017-12-17 19:28:16 -0300

    fizz buzz mod 5 and 3

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // Code goes here
 
 // genera un numero entre 1 y 5 generado de manera aleatoria y debe ser rendondiado
-//var sign = prompt("what is your astrological sign?" );
+var numRandom;
 
 for (let index = 0; index < 10; index++) {
-    console.log(index);
+    numRandom = Math.round( Math.random() * 5 );
+    console.log(numRandom);
+    if (numRandom % 3 === 0 && numRandom != 0) {
+        alert("fizz");
+    } else if (numRandom % 5 === 0 ) {
+        alert("buzz");
+    } else {
+        console.log("My number is " + numRandom);
+    }
 }
