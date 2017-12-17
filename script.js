1962d22ed44256480a5e94a3acef170f74c46696
Rabindranath Ferreira <ferreiralox@gmail.com>
2017-12-17 19:39:10 -0300

    minor fix

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ var numRandom;
 for (let numRandom = 1; numRandom <= 100; numRandom++) {
     console.log(numRandom);
     if (numRandom % 3 === 0 ) {
-        console.log("fizz");
+        if (numRandom % 5 === 0) {
+            console.log("fizzbuzz");
+        } else {
+            console.log("fizz");
+        }
     } else if (numRandom % 5 === 0 ) {
         console.log("buzz");
-    } else if (numRandom % 3 === 0 && numRandom % 5 === 0) {
-        console.log("fizzbuzz");
     } else {
         console.log("My number is " + numRandom);
     }
