decb173e770eef9e34da5289db9ef172f3248b62
Rabindranath Ferreira <ferreiralox@gmail.com>
2017-12-17 19:18:42 -0300

    no prompt

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Code goes here
 
 // genera un numero entre 1 y 5 generado de manera aleatoria y debe ser rendondiado
-var sign = prompt("what is your astrological sign?" );
+//var sign = prompt("what is your astrological sign?" );
 
 for (let index = 0; index < 10; index++) {
     console.log(index);
