1de608fab10c03e3db58698218ebf048be9b35a4
Rabindranath Ferreira <ferreiralox@gmail.com>
2017-12-17 21:07:00 -0300

    creating a zombieGame survivor

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
-// Code goes here
+// zombie game
 
-// confirm
-// if select "OK" get true
-// if select "cancel" get false
-
-var friends = [];
-
-do {
-    var person = prompt("introduzca el nombre de su nuevo amigo");
-    friends.push(person);
-    var finish = confirm("agregamos un nuevo amigo?")
-} while (finish);
-
-function friend_list(friends) {
-    console.log("List of my friends");
-
-    for (let index = 0; index < friends.length; index++) {
-        console.log (friends[index]+ "\n");
-    }
+function randomNumber(range) {
+    return Math.round ( Math.random() * range );
 }
-   
-friend_list(friends);
+
+function random_weapon(weapons,id_weapon) {
+    return weapons[randomNumber(id_weapon)];
+}
+
+var lifePoints = randomNumber(5);
+
+var beginScenarios = ["hospital","school","highway","house","apartarment"];
+var listWeapons = ["gun","bow","bomb","knife","axe","nuclear bomb"];
+var typeZombie = ["litle zombie","dog zombie","cat zombie","snake zombie"];
+
+function zombieGame(player_name,lifePoints,zombie_death) {
+    do {
+        
+    } while (condition);
+
+    alert("You win the game");
+    return true;
+}
+
+alert("The zombie game begins");
+var player_name = prompt("Introduzca su nombre");
+alert("Inicia con " + lifePoints + " puntos de vida" );
+alert("Generando escenario de forma aleatoria");
+alert("Su escenario para sobrevivir es " + beginScenarios[randomNumber(beginScenarios.length - 1)]);
+
+alert("your mission is kill 5 zombies to win");
