79152a3d82d48245e670d1a481241d723363e27d
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-01-12 00:37:21 -0300

    add constructor function, using new keyword and methods with this scope

--- js/creationalConstructorPatterns.js
+++ js/creationalConstructorPatterns.js
@@ -1,2 +1,26 @@
 // do not use deprecated js code
 'use strict';
+
+var Task = function (name) {
+    this.name = name;
+    this.completed = false;
+
+    this.complete = function(){
+        this.completed = true;
+    }
+
+    this.save = function () {
+        console.log("saving task " + this.name);
+    }
+}
+
+// instancias
+var task1 = new Task("Create demo for constructors");
+var task2 = new Task("Create demo for modules");
+var task3 = new Task("Create demo for singleton");
+var task4 = new Task("Create demo for prototypes");
+
+task1.complete();
+task2.save();
+task3.save();
+task4.save();
