8760785cfe9612dc2aac107ad1a21260987c6d9d
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-01-12 01:12:03 -0300

    created module parent, just One instace and U will use just with the methods

--- js/creationalModulePatterns.js
+++ js/creationalModulePatterns.js
@@ -1,2 +1,30 @@
 // do not use deprecated js code
 'use strict';
+
+// using for a class who was a group of methods for making a microservice, or using methos to manipulate the objet
+/*
+OBJECT LITERAL
+Se utiliza una unica instancia de la class con el patron de modulo, y se opera a traves de sus servicios
+var Name = function() {
+    method1: function(){
+        body
+    }
+    methodn: function(){
+        body
+    }
+
+}
+*/
+
+var module = function() {
+    var privateVar = "data";
+
+    return {
+        m1(){
+
+        },
+        m2(){
+            
+        }
+    }
+}
