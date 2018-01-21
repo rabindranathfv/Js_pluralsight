8bc040c934e29ef7ed29556341a7c84b0d017d33
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-01-21 19:16:35 -0300

    add using of factory patter and singleton patter
    Signed-off-by: Rabindranath Ferreira <ferreiralox@gmail.com>

--- js/creationalFactoryPatterns.js
+++ js/creationalFactoryPatterns.js
@@ -1,2 +1,6 @@
 // do not use deprecated js code
 'use strict';
+
+// using for a simplifies object creation
+// creating different objects based on need (custimisable)
+
--- js/creationalSingletonPatterns.js
+++ js/creationalSingletonPatterns.js
@@ -1,2 +1,4 @@
 // do not use deprecated js code
 'use strict';
+
+// using to check if the instance was create one time, and prevent just one object of that type
