ef0ac7317e07e3fa64c41f967aeacd2449c41315
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-02-08 02:18:57 -0300

    observable properties js

---
+++ js/book.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var Book = function (name,price){
+    // sino se retorna nada, la funcion por si misma retornara undefined
+    this.name = function (val) {
+        return this;
+    };
+
+    this.price = function (val) {
+        return this;
+    };
+
+    this.onPriceChanging = function (callback) {
+        callback(price);
+        return this;
+    };
+    
+    this.onPriceChanged = function (callback) {
+        callback(price);
+        return this;
+    };
+}
+
+var books = new Book('js methods as a functions', 24200);
+
+console.log('the name of the book is ' + books.name);
+console.log('the price of the book is $' + books.price);
+
+books.onPriceChanging(function (b,price) {
+    if (price > 100) {
+        console.log('error');
+        return false;
+    }
+    return true;
+
+});
+
+books.onPriceChanged(function (b) {
+   console.log('thi price has changed ' + b.price()); 
+});
+
+
+books.price(20);
+
+books.price(200);
