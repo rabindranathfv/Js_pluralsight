13f4f4e1062fa91873b6a1ba18e9a19ce450e375
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-02-08 01:56:58 -0300

    rabindranathfv

---
+++ js/calcu.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var calc = function (start) {
+    this.add = function (x) {
+        
+    };
+
+    this.multiply = function (y) {
+        
+    };
+
+    this.equals = function (callback) {
+        
+    };
+}
+
+new calc(0);
+    .add(1);
+    .add(2);
+    .multiply(3);
+    .equals(function (result) {
+        console.log(" "+ result);
+    });
--- js/structuralFlyweightPatterns.js
+++ js/structuralFlyweightPatterns.js
@@ -1,2 +1,3 @@
 // do not use deprecated js code
 'use strict';
+//conserves memory by sharing  portions of and objetc between objects
